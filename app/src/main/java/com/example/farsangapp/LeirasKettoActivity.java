b23170a2a56a7c5fe075f14f0bab1ddbdcc92c12
Monesz <balimn13@gmail.com>
2023-02-21 13:41:37 +0100

    added first question logic

---
+++ app/src/main/java/com/example/farsangapp/LeirasKettoActivity.java
@@ -0,0 +1,4 @@
+package com.example.farsangapp;
+
+public class LeirasKettoActivity {
+}
--- app/src/main/java/com/example/farsangapp/QuizzFirstactivity.java
+++ app/src/main/java/com/example/farsangapp/QuizzFirstactivity.java
@@ -3,14 +3,47 @@ package com.example.farsangapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class QuizzFirstactivity extends AppCompatActivity {
+    boolean farsangFarkaChecked;
+    boolean rioiKarnevalChecked;
+    boolean mohacsiBusojarasChecked;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quizz_first_page);
+        CheckBox farsangFarka = findViewById(R.id.farsang_farka);
+        CheckBox rioiKarneval = findViewById(R.id.rioi_karneval);
+        CheckBox mohacsiBusojaras = findViewById(R.id.mohacsi_busojaras);
+        Button nextButton = findViewById(R.id.next);
+        TextView errorText = findViewById(R.id.error);
+        errorText.setVisibility(View.INVISIBLE);
 
+
+        nextButton.setOnClickListener(view ->{
+        if (farsangFarka.isChecked()){
+            farsangFarkaChecked = true;
+        } else if (rioiKarneval.isChecked()) {
+            rioiKarnevalChecked = true;
+        } else if (mohacsiBusojaras.isChecked()) {
+            mohacsiBusojarasChecked = true;
+        }
+        else{
+            errorText.setVisibility(View.VISIBLE);
+            farsangFarkaChecked = false;
+            rioiKarnevalChecked = false;
+            mohacsiBusojarasChecked = false;
+        }
+        });
 
     }
 }
